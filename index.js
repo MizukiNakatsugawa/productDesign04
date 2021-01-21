2ea04d12c82b7dfce2037787a886bc4898fcf95e
MizukiNakatsugawa <nakatsugawa@romantech.co.jp>
2021-01-21 14:03:24 +0900

    commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function onClick() {
+
+let pokeyBtn = document.getElementById("pokey");
+let chocopaiBtn = document.getElementById("chocopai");
+
+// pokeyBtn.onclick = function() {
+//     unityInstance.SendMessage('TestManager', 'SetPokey');
+//     unityInstance.SendMessage('TestManager', 'SetActivePokeyLine');
+// }
+
+pokeyBtn.addEventListener("click", () => {
     unityInstance.SendMessage('TestManager', 'SetPokey');
     unityInstance.SendMessage('TestManager', 'SetActivePokeyLine');
-}
+});
 
-let chocopaiBtn = document.getElementById("chocopai");
 chocopaiBtn.addEventListener("click", () => {
-    console.log(chocopaiBtn);
     unityInstance.SendMessage('TestManager', 'SetNonActivePokeyLine');
 });
