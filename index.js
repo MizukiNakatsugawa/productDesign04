4f0f41cd42d1f243c9e8b754849e8e1a478b7493
MizukiNakatsugawa <nakatsugawa@romantech.co.jp>
2021-01-27 05:15:23 +0900

    commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
 let pokeyBtn = document.getElementById("pokey");
 let chocopaiBtn = document.getElementById("chocopai");
-
-// pokeyBtn.onclick = function() {
-//     unityInstance.SendMessage('TestManager', 'SetPokey');
-//     unityInstance.SendMessage('TestManager', 'SetActivePokeyLine');
-// }
+let checkbox = document.getElementById("checkbox");
+let cancelBtn = document.getElementById("cancel");
+let infoBtn = document.getElementById("infotmationBtn");
+let howtouseCancelBtn = document.getElementById("howtouseCancel");
 
 pokeyBtn.addEventListener("click", () => {
     unityInstance.SendMessage('TestManager', 'SetPokey');
@@ -15,3 +14,28 @@ pokeyBtn.addEventListener("click", () => {
 chocopaiBtn.addEventListener("click", () => {
     unityInstance.SendMessage('TestManager', 'SetNonActivePokeyLine');
 });
+
+checkbox.addEventListener("change", () => {
+    isChecked();
+});
+
+cancelBtn.addEventListener("click", () => {
+    checkbox.checked = false;
+    isChecked();
+});
+
+function isChecked() {
+    if (checkbox.checked) {
+        document.getElementById("sidebar").style.left = "0";
+    } else {
+        document.getElementById("sidebar").style.left = "-500px";
+    }
+}
+
+infoBtn.addEventListener("click", () => {
+    document.getElementById("howtouse").style.right = "0";
+});
+
+howtouseCancelBtn.addEventListener("click", () => {
+    document.getElementById("howtouse").style.right = "-500px";
+})
