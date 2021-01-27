0b9e8d0cd6c6bc9c0d827d11c2f8303b6cdb2c44
MizukiNakatsugawa <nakatsugawa@romantech.co.jp>
2021-01-28 05:04:03 +0900

    commit

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 
 let pokeyBtn = document.getElementById("pokey");
 let chocopaiBtn = document.getElementById("chocopai");
+let alfortBtn = document.getElementById("alfort");
+let almondBtn = document.getElementById("almondchoco");
+let blackBtn = document.getElementById("blackthunder");
+let koedaBtn = document.getElementById("koeda");
+let pienomiBtn = document.getElementById("pienomi");
+let takenokoBtn = document.getElementById("takenokonosato");
+
 let checkbox = document.getElementById("checkbox");
 let cancelBtn = document.getElementById("cancel");
 let infoBtn = document.getElementById("infotmationBtn");
 let howtouseCancelBtn = document.getElementById("howtouseCancel");
 
+
 pokeyBtn.addEventListener("click", () => {
-    unityInstance.SendMessage('TestManager', 'SetPokey');
-    unityInstance.SendMessage('TestManager', 'SetActivePokeyLine');
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Pokey');
 });
 
 chocopaiBtn.addEventListener("click", () => {
-    unityInstance.SendMessage('TestManager', 'SetNonActivePokeyLine');
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Chocopie');
+});
+
+alfortBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Alfort');
+});
+
+almondBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Almond');
+});
+
+blackBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Black');
+});
+
+koedaBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Koeda');
+});
+
+pienomiBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Pienomi');
+});
+
+takenokoBtn.addEventListener("click", () => {
+    unityInstance.SendMessage('TestManager', 'SetObject', 'Takenoko');
 });
 
 checkbox.addEventListener("change", () => {
